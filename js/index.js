5cd83cfa68a8439a486ff854e648af42d9129249
Chen0089 <108268190+Chen0089@users.noreply.github.com>
2024-09-17 17:07:34 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,14 @@
-function activate(element) {
-    document.querySelectorAll('.nav a').forEach(link => link.classList.remove('active'));
-    element.classList.add('active');
+const textContainer = document.getElementById('text-container');
+const texts = ['Hello world!!!', 'welcome to my website!', 'have a nice day!'];
+
+function randomText() {
+  const randomIndex = Math.floor(Math.random() * texts.length);
+  return texts[randomIndex];
 }
+
+function updateText() {
+  const span = textContainer.querySelector('span');
+  span.textContent = randomText();
+}
+
+setInterval(updateText, 1000);
