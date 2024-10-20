4801c1a1dc811f28f9cef60b76d17c81fe6c53de
Chen0089 <108268190+Chen0089@users.noreply.github.com>
2024-10-20 17:20:43 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -12,3 +12,17 @@ function updateText() {
 }
 
 setInterval(updateText, 1000);
+
+fetch('projects.json')
+    .then(response => response.json())
+    .then(data => {
+        // 随机选择一个项目
+        const randomIndex = Math.floor(Math.random() * data.length);
+        const project = data[randomIndex];
+
+        // 更新页面内容
+        document.getElementById('project-name').innerText = project.name + " (Star: " + project.stars + "+)";
+        document.getElementById('project-description').innerText = project.description;
+        document.getElementById('project-link').setAttribute('href', project.url);
+    })
+    .catch(error => console.error('Error fetching project data:', error));
