ed61acd4873cc22b09e41ac5b5167bac18983d46
Chen0089 <108268190+Chen0089@users.noreply.github.com>
2024-11-30 19:14:19 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    const starsThreshold = 10000;
+    const starsThreshold = 1000;
     const githubSearchApiUrl = `https://api.github.com/search/repositories?q=stars:>${starsThreshold}&per_page=10`;
 
     fetch(githubSearchApiUrl)
         .then(response => {
             if (!response.ok) {
-                throw new Error('Network response was not ok ' + response.statusText);
+                throw new Error('网络似乎有点问题...错误代码:' + response.statusText);
             }
             return response.json();
         })
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
             const repoList = document.getElementById('repo-list');
             data.items.forEach(repo => {
                 const listItem = document.createElement('li');
-                listItem.textContent = `Repository: ${repo.name}, Stars: ${repo.stargazers_count}, URL: ${repo.html_url}`;
+                listItem.textContent = `名称: ${repo.name}, 星数: ${repo.stargazers_count}, 链接: ${repo.html_url}`;
                 repoList.appendChild(listItem);
             });
         })
