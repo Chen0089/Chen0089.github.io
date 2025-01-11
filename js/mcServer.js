45ee7249d4c2033643f377e69fdb0483f413e51b
Chen0089 <108268190+Chen0089@users.noreply.github.com>
2025-01-11 20:17:06 +0800

    Update mcServer.js

--- js/mcServer.js
+++ js/mcServer.js
@@ -1,3 +1,6 @@
+setTimeout(function() {
+            window.location.href = "https://www.example.com";
+        }, 5000); // 5秒后重定向
 document.addEventListener("DOMContentLoaded", function (event) {
           reloadTheme("index.html");
           initLang(0);
