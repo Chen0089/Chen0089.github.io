e1ec6817981a4c8035a16ea17d956c9c7ba9343e
Chen0089 <108268190+Chen0089@users.noreply.github.com>
2025-04-22 19:12:56 +0800

    index已拆分QWQ

---
+++ js/index/fps.js
@@ -0,0 +1,19 @@
+var fpsElement = document.getElementById('fps');
+		var frameCount = 0;
+		var fps = 0;
+
+function updateFPS() {
+    frameCount++;
+    var currentTime = Date.now();
+    var deltaTime = currentTime - lastTime;
+    if (deltaTime >= 1000) {
+        fps = Math.round(frameCount / (deltaTime / 1000));
+        frameCount = 0;
+        lastTime = currentTime;
+    }
+    fpsElement.textContent = '帧率（FPS）: ' + fps;
+    requestAnimationFrame(updateFPS);
+}
+
+var lastTime = Date.now();
+requestAnimationFrame(updateFPS);
